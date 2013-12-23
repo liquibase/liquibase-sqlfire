94e58d4bfde73c297aacc949ede4188f85f9ce19
Jeffrey Williams <jeffrey.williams@adp.com>
2013-12-22 21:57:56 -0600

    Initial import of code to support sqlfire in liquibase.

--- README.md
+++ README.md
@@ -1,4 +1,75 @@
-liquibase-sqlfire
-=================
+##Liquibase SqlFire Plugin##
+This adds support for the Pivotal's SqlFire database to Liquibase. 
 
-Liquibase plugin to provide support for Pivitol's SqlFire database.
+SqlFire is an in-memory distributed SQL database. In the category of NewSQL databases, SQLFire delivers dynamic scalability and high performance for data-intensive modern applications. 
+
+SqlFire provides it's sql interface by using a port of Apache Derby.
+
+
+###How to Use###
+To enable this plugin it needs to be added to the classpath prior to the liquibase.jar. This is due to having to override a couple of datatypes with in liquibase-core project. Therefore it is required that this module be picked up first from the classpath.
+
+Another gotcha is that SqlFire reports it's Database Product Name as "Apache Derby". The Priority for SqlFireDatabase has been set to **PRIORITY_DATABASE** and should be picked up before the DerbyDatabse implementation.
+
+The simplest way is to add this to the Liquibase distribution's "lib" folder and adjust the shell script to add jars from the lib folder before the liquibase*.jar.
+
+
+###Supported Refactoring/Changes###
+With SqlFire there are two layers where a Liquibase refactor may not be supported. As SqlFire uses Apache Derby as it's sql interface a method may not supported at the Apache Derby. It is also possible that the underlying SqlFire Database may not support certain features.  
+
+Generally the SqlFire features is a subset of those support by Apache Derby.  
+
+N=Not supported
+Y=Supported
+?=Untried
+
+Liquibase Refactoring|Derby|SqlFire|Description
+:--------------------|:---:|:-----:|:----------
+ADD AUTO INCREMENT			|N|N|Not supported by Derby
+ADD COLUMN					|Y|Y| 
+ADD DEFAULT VALUE			|Y|N|Column Modification not supported by SqlFire 
+ADD FOREIGN KEY CONSTRAINT	|Y|?| 
+ADD LOOKUP TABLE			|Y|Y| 
+ADD NOT NULL CONSTRAINT		|Y|N|Column Modification not supported by SqlFire
+ADD PRIMARY KEY				|Y|Y| 
+ADD UNIQUE CONSTRAINT		|Y|Y| 
+ALTER SEQUENCE				|N|N|Sequence not supported by Derby 
+CREATE INDEX				|N|N|Sequence not supported by Derby 
+CREATE PROCEDURE			|Y|Y| 
+CREATE SEQUENCE				|N|N|Sequence not supported by Derby 
+CREATE TABLE				|Y|Y| 
+CREATE VIEW					|Y|Y| 
+CUSTOM CHANGE				|Y|Y| 
+DELETE						|Y|Y| 
+DROP ALL FOREIGN KEY CONSTRAINTS	|Y|?| 
+DROP COLUMN					|Y|Y| 
+DROP DEFAULT VALUE			|Y|N|Column Modification not supported by SqlFire 
+DROP FOREIGN KEY CONSTRAINT			|Y|?| 
+DROP INDEX					|Y|Y| 
+DROP NOT NULL CONSTRAINT	|Y|N|Column Modification not supported by SqlFire 
+DROP PRIMARY KEY			|Y|Y| 
+DROP SEQUENCE				|Y|Y| 
+DROP TABLE					|Y|Y| 
+DROP UNIQUE CONSTRAINT		|Y|Y| 
+DROP VIEW					|Y|Y| 
+EMPTY						|Y|Y| 
+EXECUTE COMMAND				|Y|?| 
+INSERT						|Y|Y| 
+LOAD DATA					|Y|?| 
+LOAD UPDATE DATA			|Y|?| 
+MERGE COLUMNS				|Y|N|Column Modification not supported by SqlFire
+MODIFY DATA TYPE			|Y|N|Column Modification not supported by SqlFire
+RENAME COLUMN				|Y|N|RENAME statement not yet implemented by SqlFire.
+RENAME TABLE				|Y|N|RENAME statement not yet implemented by SqlFire. 
+RENAME VIEW					|N|N|RENAME statement not yet implemented by SqlFire.
+SQL							|Y|?| 
+SQL FILE					|Y|?| 
+STOP						|Y|Y| 
+TAG DATABASE				|Y|Y| 
+UPDATE						|Y|Y| 
+
+
+
+
+
+
---
+++ src/main/java/liquibase/database/ext/SQLFireDatabase.java
@@ -0,0 +1,154 @@
+package liquibase.database.ext;
+
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Enumeration;
+
+import liquibase.CatalogAndSchema;
+import liquibase.database.AbstractJdbcDatabase;
+import liquibase.database.Database;
+import liquibase.database.DatabaseConnection;
+import liquibase.exception.DatabaseException;
+import liquibase.executor.Executor;
+import liquibase.executor.ExecutorService;
+import liquibase.logging.LogFactory;
+import liquibase.statement.core.CreateDatabaseChangeLogLockTableStatement;
+import liquibase.statement.core.DropTableStatement;
+import liquibase.statement.core.InitializeDatabaseChangeLogLockTableStatement;
+import liquibase.statement.core.RawSqlStatement;
+import liquibase.structure.DatabaseObject;
+
+public class SQLFireDatabase extends AbstractJdbcDatabase {
+
+	public SQLFireDatabase() {
+        super.setCurrentDateTimeFunction("CURRENT_TIMESTAMP");
+        super.sequenceNextValueFunction = "NEXT VALUE FOR %s";
+        super.sequenceCurrentValueFunction = "(SELECT currentvalue FROM sys.syssequences WHERE %s='SEQ_TYPE')";
+	}
+
+    @Override
+    public boolean isCorrectDatabaseImplementation(DatabaseConnection conn) throws DatabaseException {
+        return "Apache Derby".equalsIgnoreCase(conn.getDatabaseProductName())
+        		&& conn.getURL().startsWith("jdbc:sqlfire:");
+    }
+
+    @Override
+    public String getDefaultDriver(String url) {
+    	if(url.startsWith("jdbc:sqlfire://")){
+    		return "com.vmware.sqlfire.jdbc.ClientDriver";
+    	}
+    	return null;
+    }
+
+    @Override
+    public int getPriority() {
+        return PRIORITY_DATABASE;
+    }
+
+    @Override
+    public boolean supportsSchemas() {
+        return false;
+    }
+
+    @Override
+    public boolean jdbcCallsCatalogsSchemas() {
+        return true;
+    }
+
+    @Override
+    public Integer getDefaultPort() {
+        return 1527;
+    }
+
+    @Override
+    protected String getDefaultDatabaseProductName() {
+        return "SqlFire";
+    }
+
+    @Override
+    public String correctObjectName(String objectName, Class<? extends DatabaseObject> objectType) {
+        if (objectName == null) {
+            return null;
+        }
+        return objectName.toUpperCase();
+    }
+
+    @Override
+    public String getShortName() {
+        return "sqlfire";
+    }
+
+    @Override
+    public boolean supportsSequences() {
+    	return false;
+    }
+
+    @Override
+    public boolean supportsInitiallyDeferrableColumns() {
+        return false;
+    }
+
+    @Override
+    public String getDateLiteral(String isoDate) {
+        if (isDateOnly(isoDate)) {
+            return "DATE(" + super.getDateLiteral(isoDate) + ")";
+        } else if (isTimeOnly(isoDate)) {
+            return "TIME(" + super.getDateLiteral(isoDate) + ")";
+        } else {
+            String dateString = super.getDateLiteral(isoDate);
+            int decimalDigits = dateString.length() - dateString.indexOf('.') - 2;
+            String padding = "";
+            for (int i=6; i> decimalDigits; i--) {
+                padding += "0";
+            }
+            return "TIMESTAMP(" + dateString.replaceFirst("'$", padding+"'") + ")";
+        }
+    }
+
+    @Override
+    public boolean supportsTablespaces() {
+        return false;
+    }
+
+    @Override
+    public String getViewDefinition(CatalogAndSchema schema, String name) throws DatabaseException {
+        return super.getViewDefinition(schema, name).replaceFirst("CREATE VIEW \\w+ AS ", "");
+    }
+
+    @Override
+    protected String getConnectionCatalogName() throws DatabaseException {
+        try {
+            return ExecutorService.getInstance().getExecutor(this).queryForObject(new RawSqlStatement("select current schema from sysibm.sysdummy1"), String.class);
+        } catch (Exception e) {
+            LogFactory.getLogger().info("Error getting default schema", e);
+        }
+        return null;
+    }
+
+    @Override
+    public boolean supportsCatalogInObjectName(Class<? extends DatabaseObject> type) {
+        return true;
+    }
+
+    @Override
+    public void checkDatabaseChangeLogLockTable() throws DatabaseException {
+        super.checkDatabaseChangeLogLockTable();
+
+        if (this.supportsBooleanDataType()) { //check if the changelog table is of an old smallint vs. boolean format
+            Executor executor = ExecutorService.getInstance().getExecutor(this);
+            String lockTable = this.escapeTableName(this.getLiquibaseCatalogName(), this.getLiquibaseSchemaName(), this.getDatabaseChangeLogLockTableName());
+            Object obj = executor.queryForObject(new RawSqlStatement("select min(locked) as test from " + lockTable + " fetch first row only"), Object.class);
+            if (!(obj instanceof Boolean)) { //wrong type, need to recreate table
+                executor.execute(new DropTableStatement(this.getLiquibaseCatalogName(), this.getLiquibaseSchemaName(), this.getDatabaseChangeLogLockTableName(), false));
+                executor.execute(new CreateDatabaseChangeLogLockTableStatement());
+                executor.execute(new InitializeDatabaseChangeLogLockTableStatement());
+            }
+        }
+    }
+
+    public boolean supportsBooleanDataType() {
+    	return false;
+    }
+
+}
