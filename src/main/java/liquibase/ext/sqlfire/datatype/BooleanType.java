da73653391797575378dcc8f3a1380ce7be3f747
Nathan Voxland <nathan@voxland.net>
2014-06-02 12:02:05 -0500

    Updated for liquibase 3.2.0 final

--- src/main/java/liquibase/ext/sqlfire/datatype/BooleanType.java
+++ src/main/java/liquibase/ext/sqlfire/datatype/BooleanType.java
@@ -1,7 +1,6 @@
 package liquibase.ext.sqlfire.datatype;
 
 import liquibase.database.Database;
-import liquibase.database.core.CacheDatabase;
 import liquibase.database.core.DB2Database;
 import liquibase.database.core.DerbyDatabase;
 import liquibase.database.core.FirebirdDatabase;
@@ -27,9 +26,7 @@ public class BooleanType extends LiquibaseDataType {
 
     @Override
     public DatabaseDataType toDatabaseDataType(Database database) {
-        if (database instanceof CacheDatabase) {
-            return new DatabaseDataType("INT");
-        } else if (database instanceof DB2Database || database instanceof FirebirdDatabase) {
+       if (database instanceof DB2Database || database instanceof FirebirdDatabase) {
             return new DatabaseDataType("SMALLINT");
         } else if (database instanceof MSSQLDatabase) {
             return new DatabaseDataType("BIT");
@@ -103,8 +100,7 @@ public class BooleanType extends LiquibaseDataType {
         if (database instanceof DerbyDatabase) {
             return !((DerbyDatabase) database).supportsBooleanDataType();
         }
-        return database instanceof CacheDatabase
-                || database instanceof DB2Database
+        return database instanceof DB2Database
                 || database instanceof FirebirdDatabase
                 || database instanceof MSSQLDatabase
                 || database instanceof MySQLDatabase
